cca21f9480b5e78b1938d07bab73163a12811666
Vijay Pandey <Vijay.Pandey@synapse.com>
2018-11-26 16:22:31 +0530

    spelling mistakes :)

--- src/Components/Common/faqs.js
+++ src/Components/Common/faqs.js
@@ -5,7 +5,7 @@ export function Faqs() {
             <a href="#" className="list-group-item ">Luke Skywalker has unlimited access.</a>
             <a href="#" className="list-group-item ">Others will get 15 seacrch in 60 seconds.</a>
             <a href="#" className="list-group-item ">Timer and search count will persist in-memory storage(no redux as of now).</a>
-            <a href="#" className="list-group-item ">Storage is maintained by store han will work even we visit a planed details and come back.</a>
+            <a href="#" className="list-group-item ">Storage is maintained by store hance will work even we visit a planet details and come back.</a>
         </div>
     </div>);
 }
